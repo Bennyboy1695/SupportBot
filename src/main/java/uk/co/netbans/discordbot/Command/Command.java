b7cfcd57d774f8111de6c2e2cb8808456beebd30
Matt Worzala <WorzalaM02@gmail.com>
2018-12-07 20:52:56 -0500

    more fix

--- src/main/java/uk/co/netbans/discordbot/Command/Command.java
+++ src/main/java/uk/co/netbans/discordbot/Command/Command.java
@@ -2,10 +2,9 @@ package uk.co.netbans.discordbot.Command;
 
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.TextChannel;
-import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import uk.co.netbans.discordbot.NetBansBot;
 
-public interface Command<T, M extends ListenerAdapter> {
+public interface Command {
     CommandResult onExecute(NetBansBot bot, Member sender, TextChannel channel, String label, String[] args);
 
     String name();
