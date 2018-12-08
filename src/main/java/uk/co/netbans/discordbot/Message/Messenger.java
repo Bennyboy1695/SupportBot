c8be1abbdd35a9d9c405fbb2fbdb8485ee54374f
Bennyboy1695 <benny.boy1695@gmail.com>
2018-12-08 00:10:43 +0000

    Added Private Messages and Used correct Embed Object

--- src/main/java/uk/co/netbans/discordbot/Message/Messenger.java
+++ src/main/java/uk/co/netbans/discordbot/Message/Messenger.java
@@ -3,7 +3,9 @@ package uk.co.netbans.discordbot.Message;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageEmbed;
 import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -24,12 +26,12 @@ public class Messenger {
         this.sendMessage(channel, new MessageBuilder().append(message).build(), lifetime);
     }
 
-    public void sendEmbed(TextChannel channel, EmbedBuilder embed) {
-        this.sendMessage(channel, new MessageBuilder().setEmbed(embed.build()).build(), 0);
+    public void sendEmbed(TextChannel channel, MessageEmbed embed) {
+        this.sendMessage(channel, new MessageBuilder().setEmbed(embed).build(), 0);
     }
 
-    public void sendEmbed(TextChannel channel, EmbedBuilder embed, int lifetime) {
-        this.sendMessage(channel, new MessageBuilder().setEmbed(embed.build()).build(), lifetime);
+    public void sendEmbed(TextChannel channel, MessageEmbed embed, int lifetime) {
+        this.sendMessage(channel, new MessageBuilder().setEmbed(embed).build(), lifetime);
     }
 
     public void sendMessage(TextChannel channel, Message message, int lifetime) {
@@ -37,5 +39,13 @@ public class Messenger {
 
         if (lifetime != 0)
             this.executor.schedule(() -> message.delete().reason("netbans_message_autoremoval").complete(), lifetime, TimeUnit.SECONDS);
+    }
+
+    public void sendPrivateMessage(User user, MessageEmbed embed) {
+        user.openPrivateChannel().queue( (channel) -> channel.sendMessage(embed).complete() );
+    }
+
+    public void sendPrivateMessage(User user, Message message) {
+        user.openPrivateChannel().queue( (channel) -> channel.sendMessage(message).complete() );
     }
 }
