946ad83b111cfd7dee5513badab65a11f52a64a0
Bennyboy1695 <benny.boy1695@gmail.com>
2018-12-26 19:20:33 +0000

    Movable channels

--- src/main/java/uk/co/netbans/supportbot/Support/Command/Admin/ManualChannel.java
+++ src/main/java/uk/co/netbans/supportbot/Support/Command/Admin/ManualChannel.java
@@ -17,39 +17,47 @@ public class ManualChannel implements Command {
 
     @Override
     public CommandResult onExecute(NetBansBot bot, Member sender, TextChannel channel, String label, String[] args) {
-        if (args.length >= 1) {
-            DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/YY");
-            DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
-            TextChannel supportChannel;
+        if (args.length >= 2) {
+            if (args[1].equals("create")) {
+                DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/YY");
+                DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
+                TextChannel supportChannel;
 
-            Member member = bot.getJDA().getGuildById(Long.valueOf((String) bot.getConf().get("guildID"))).getMemberById(args[0].replaceAll("<", "").replaceAll("@", "").replaceAll("!", "").replaceAll(">", ""));
-            if (args.length != 2) {
-                supportChannel = (TextChannel) bot.getJDA().getCategoryById(Long.valueOf((String) bot.getConf().get("category")))
-                        .createTextChannel(member.getEffectiveName() + "-" + "manual").complete();
+                Member member = bot.getJDA().getGuildById(Long.valueOf((String) bot.getConf().get("guildID"))).getMemberById(args[1].replaceAll("<", "").replaceAll("@", "").replaceAll("!", "").replaceAll(">", ""));
+                if (args.length != 3) {
+                    supportChannel = (TextChannel) bot.getJDA().getCategoryById(Long.valueOf((String) bot.getConf().get("category")))
+                            .createTextChannel(member.getEffectiveName() + "-" + "manual").complete();
+                } else {
+                    supportChannel = (TextChannel) bot.getJDA().getTextChannelById(args[2].replaceAll("<", "").replaceAll("#", "").replaceAll(">", "")).getParent().createTextChannel(member.getEffectiveName() + "-" + "manual").complete();
+                }
+
+                supportChannel.createPermissionOverride(member).setAllow(101440).complete();
+                supportChannel.getManager().setTopic("Creation date: " + supportChannel.getCreationTime().format(dateFormat) + " Creation Time: " + supportChannel.getCreationTime().format(timeFormat) + "GMT").complete();
+                Message message = new MessageBuilder()
+                        .append("**User:** " + member.getAsMention())
+                        .append("\n")
+                        .append("**Message:** " + "This channel was created manually, so please see below messages for the reason!")
+                        .append("\n")
+                        .append("\n")
+                        .append("_To close this ticket please react with a \u2705 to this message!_")
+                        .build();
+                Message supportMessage = bot.getMessenger().sendMessage(supportChannel, message, 0);
+                supportMessage.pin().complete();
+                supportChannel.getHistory().retrievePast(1).queue(l -> l.forEach(m -> m.delete().queue()));
+                supportMessage.addReaction("\u2705").complete();
+                member.getUser().openPrivateChannel().complete().sendMessage(new EmbedBuilder()
+                        .setTitle("Support Channel")
+                        .setDescription("https://discordapp.com/channels/" + bot.getConf().get("guildID") + "/" + supportChannel.getIdLong())
+                        .setColor(new Color(127, 255, 212))
+                        .build()).complete();
+                return CommandResult.SUCCESS;
+            } else if (args[0].equals("move")) {
+                TextChannel channelArg = bot.getJDA().getTextChannelById(args[1].replaceAll("<", "").replaceAll("#", "").replaceAll(">", ""));
+                channel.getManager().setParent(channelArg.getParent()).complete();
+                return CommandResult.SUCCESS;
             } else {
-                supportChannel = (TextChannel) bot.getJDA().getTextChannelById(args[1].replaceAll("<", "").replaceAll("#", "").replaceAll(">", "")).getParent().createTextChannel(member.getEffectiveName() + "-" + "manual").complete();
+                return CommandResult.INVALIDARGS;
             }
-
-            supportChannel.createPermissionOverride(member).setAllow(101440).complete();
-            supportChannel.getManager().setTopic("Creation date: " + supportChannel.getCreationTime().format(dateFormat) + " Creation Time: " + supportChannel.getCreationTime().format(timeFormat) + "GMT").complete();
-            Message message = new MessageBuilder()
-                    .append("**User:** " + member.getAsMention())
-                    .append("\n")
-                    .append("**Message:** " + "This channel was created manually, so please see below messages for the reason!")
-                    .append("\n")
-                    .append("\n")
-                    .append("_To close this ticket please react with a \u2705 to this message!_")
-                    .build();
-            Message supportMessage = bot.getMessenger().sendMessage(supportChannel, message, 0);
-            supportMessage.pin().complete();
-            supportChannel.getHistory().retrievePast(1).queue(l -> l.forEach(m -> m.delete().queue()));
-            supportMessage.addReaction("\u2705").complete();
-            member.getUser().openPrivateChannel().complete().sendMessage(new EmbedBuilder()
-                    .setTitle("Support Channel")
-                    .setDescription("https://discordapp.com/channels/" + bot.getConf().get("guildID") + "/" + supportChannel.getIdLong())
-                    .setColor(new Color(127, 255, 212))
-                    .build()).complete();
-            return CommandResult.SUCCESS;
         }
         return CommandResult.INVALIDARGS;
     }
@@ -66,7 +74,7 @@ public class ManualChannel implements Command {
 
     @Override
     public String usage() {
-        return "channel <user> [channel]";
+        return "channel <create|move> <user|channel> [channel]";
     }
 
     @Override
